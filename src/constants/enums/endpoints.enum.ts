dfd5504eefc0236364202fe2fbe4f6a874ef1895
Andrew <gandrejko2002@gmail.com>
2023-07-21 10:24:13 +0300

    * added slash before endpoints

--- src/constants/enums/endpoints.enum.ts
+++ src/constants/enums/endpoints.enum.ts
@@ -1,7 +1,7 @@
 export enum Endpoints {
-    POPULAR_MOVIES = 'movie/popular/',
-    UPCOMING_MOVIES = 'movie/upcoming/',
-    TOP_RATED_MOVIES = 'movie/top_rated/',
-    SEARCH_MOVIES = 'search/movie',
-    MOVIE_BY_ID = 'movie',
+    POPULAR_MOVIES = '/movie/popular/',
+    UPCOMING_MOVIES = '/movie/upcoming/',
+    TOP_RATED_MOVIES = '/movie/top_rated/',
+    SEARCH_MOVIES = '/search/movie',
+    MOVIE_BY_ID = '/movie',
 }
